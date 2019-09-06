17ae72ce75b9cf4e3e128badf98a04cc0a5ff93d
abaiense <antonio.baiense@gmail.com>
2019-09-06 19:16:17 -0300

    Aula 03 (Fim)
    Termino da Aula 03

--- Alura/Alura_JS/Aula 03/introducao-javascript/js/principal.js
+++ Alura/Alura_JS/Aula 03/introducao-javascript/js/principal.js
@@ -1,37 +1,45 @@
 var titulo =  document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (i = 0; i < pacientes.length; i++) {
+    
+    var paciente = pacientes[i]; 
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    if (peso <= 0 || peso >= 1000){
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if (altura <= 0 || altura >= 3){
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if (pesoEhValido && alturaEhValida){
+    
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    
+    }
+    
 
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-var tdImc = paciente.querySelector(".info-imc");
-
-if (peso <= 0 || peso >= 1000){
-    console.log("Peso Inválido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
+    
 }
 
-if (altura <= 0 || altura >= 3){
-    console.log("Altura Inválido");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida";
-}
-
-if (pesoEhValido && alturaEhValida){
-
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-
-    console.log(imc);
-}
 
 
