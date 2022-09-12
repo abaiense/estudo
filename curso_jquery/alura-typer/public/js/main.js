10a30a1252c458d01f8451e390c38bf8e77eadd1
abaiense <antonio.baiense@gmail.com>
2022-09-12 12:11:07 -0300

    Aula 02 Jquery - Alura

--- curso_jquery/alura-typer/public/js/main.js
+++ curso_jquery/alura-typer/public/js/main.js
@@ -4,3 +4,17 @@ var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+
+campo.on("input", function(){
+
+    var conteudo = campo.val();
+
+    var qtdCaractere = conteudo.length;
+    /* var qtdPalavras = conteudo.split(" ").length; */
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+
+    $("#contador-palavras").text(qtdPalavras);
+    $("#contador-caracteres").text(qtdCaractere);
+});
