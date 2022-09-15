3edcf413e9b36bfa9b490466df839b88324b1794
abaiense <antonio.baiense@gmail.com>
2022-09-15 16:42:28 -0300

    Adicionando Busca Selecionada para o Alura Typer

--- curso_jquery/alura-typer/public/js/frase.js
+++ curso_jquery/alura-typer/public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria(){
 
@@ -23,5 +24,33 @@ function trocaFraseAleatoria(data){
 
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numAleatorio].tempo);
+}
+
+function buscaFrase(data){
+
+    $("#spinner").toggle();
+
+    var fraseID = $("#frase-id").val();
+    var dados = { id: fraseID};
+
+    $.get("http://localhost:3000/frases",dados, trocaFrase)
+    .fail(() => {
+        $("#erro").show();
+        setTimeout(function(){
+            $("#erro").fadeOut(600);
+        }, 2000);
+    })
+    .always(() => {
+        $("#spinner").toggle();
+    });
+
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
 
